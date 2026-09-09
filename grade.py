3a9e7e7ae5eb111112e0bd731049aeb314864eaf
Harika codes <harikamalluvalasa3@gmail.com>
2026-09-09 17:04:44 +0530

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,23 @@
+name = 'harika'
+
+m = int(input('maths mark: '))
+
+t = int(input('telugu mark: '))
+
+total = m+t
+
+avg = total/2
+
+ print(name, 'total=', total)
+
+if avg >= 90:
+
+ print('grade A TOPPER')
+
+elif avg >= 50:
+
+ print('grade B good')
+
+else:
+
+ print('grade C')
